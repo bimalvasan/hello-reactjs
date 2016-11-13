e34fa6047a3d4196bdba955f280c216ea832c06e
Bimal <bimalvasan@gmail.com>
2016-11-13 18:36:43 +0530

    First commit

---
+++ src/components/forum.react.js
@@ -0,0 +1,23 @@
+var Forum = React.createClass({
+	getInitialState: function(){
+		return {
+			allAnswers: {
+				'1': {
+					body: "Isnt that about time travel?",
+					correct: false
+				},
+				'2': {
+					body: "React and Flux are a tool?",
+					correct: false
+				},
+				'3': {
+					body: "React is a synonym for 'respond'?",
+					correct: false
+				}
+			}
+		}
+	},
+    render: function(){
+        return React.createElement('div', null, React.createElement(ForumHeader, {allAnswers: this.state.allAnswers}));
+    }
+});
